7af26716e784d78ed9fe47d83f536300e7e503c9
Mert İslam Uğurlu <mertislamugurlu@gmail.com>
2025-02-27 01:08:45 +0300

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 document.addEventListener("DOMContentLoaded", function() {
     particlesJS.load('particles-js', 'particles.json', function() {
         console.log('particles.js config loaded');
